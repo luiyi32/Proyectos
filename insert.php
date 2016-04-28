4786117c28d7e6374db03f7c8e05fba0486043d1
luiyi32 <arteagarcia95@gmail.com>
2016-04-28 17:47:54 -0500

    Update insert.php

--- insert.php
+++ insert.php
@@ -1,24 +1,30 @@
 <?php
 	include("conectar.php");
-
 ?>
 
 <form action="" method="post">
-	Titulo:<input required name="tit"><br/><br/>
-	Autor: <input  name="aut"> <br/><br/>
-	Editorial:<input  name="edit"> </br></br>
-	Anio: <input name="fech"> </br></br>
-
-<input type="submit" />
+<center>
+	<h1>Ingresar Datos de el Libro</h1><br/>
+	Titulo:<input required name="title"><br/><br/>
+	Autor:<input name="aut"><br/><br/>
+	Editorial:<input name="edit"><br/><br/>
+	Anio:<input name="fech"> </br></br>
+	SSA: <input name="sa"> </br><br>
+<input type="submit"/>
+</center>
 </form>
 
 <?php
-	if($_POST){ //validacion de datos al precionar botton
-	$ti=$_POST["tit"];
-	$au=$_POST["aut"];
-	$ed=$_POST["edit"];
+	if($_POST){
+	$t=$_POST["title"];
+	$a=$_POST["aut"];
+	$e=$_POST["edit"];
+	$f=$_POST["fech"];
+	$s=$_POST["sa"];
 	
-	mysql_query("insert into Registro(Titulo,Autor,Editorial)values('$ti','$au','$ed')")or die(mysql_error());
-echo "<h3>Datos Guardados</h3>";
+	mysql_query("insert into datos(Titulo,Autor,Editorial,Fecha,Ssa)values('$t','$a','$e','$f','$s')")or die(mysql_error());
+
+echo "<h3>Datos guardados</h3>";
+
 }
 ?>
